625c89158fbe488291ee91e8f50fdae45be20676
Tom Brouwer <tombrouwer@outlook.com>
2022-01-28 16:13:56 +0100

    Extend possibilities for YAML parsing and add examples of server config files
    The config files (scs_auth.yaml and scs_conf.yaml) describe the locations
    of directories with the configuration templates, common variables and
    secrets.
    
    All configuration files can use the !scs-relative, !scs-secret and
    !scs-common tags to reference files using a relative reference,
    reference to the secrets directory and reference to the common
    directory respectively. The !scs-expand-env tag is used to
    expand environment variables used in a string.
    
    Secrets files only support references to other secrets files, and
    allow for the !scs-gen-secret tag, which auto-generates a
    32 bytes token, and saves it back to the file. Note that comments
    will be lost in this process.

--- scripts/get_config.py
+++ scripts/get_config.py
@@ -5,34 +5,23 @@ Test script to get the configuration from a parsed configuration file
 import argparse
 from pathlib import Path
 import re
+import secrets
+import os
 
 import jinja2
 import yaml
 
 # Configure ninja2
 file_folder = Path(__file__).absolute().parent
-config_files_path = Path(file_folder, '../data/config')
-loader = jinja2.FileSystemLoader(searchpath=config_files_path)
-jinja_env = jinja2.Environment(
-    loader=loader,
-    trim_blocks=True,
-    lstrip_blocks=True
-)
-
-secrets_dir = Path(file_folder, '../data/secrets')
-common_dir = Path(file_folder, '../data/common')
 
 index_regex = re.compile(r'\[(\d+)\]')
+env_variable_pattern = re.compile(r'\$\{([^}^{]+)\}')
 
 
 # Add scs-ref tag constructor for pyyaml
 def construct_scs_ref(loader, node):
     """PyYaml constructor for scs-ref tags"""
     ref = node.value
-
-    # expand scs variables
-    ref = ref.replace('${scs-secrets}', secrets_dir.as_posix())
-    ref = ref.replace('${scs-common}', common_dir.as_posix())
 
     # TODO: How to handle references to files in the same dir?
     ref_parts = ref.split('#')
@@ -43,7 +32,17 @@ def construct_scs_ref(loader, node):
     else:
         file_path, attribute_loc = ref_parts
 
-    file_data = load_yaml(file_path)
+    # Resolve full path
+    contains_secrets = False
+    if node.tag == '!scs-secret':
+        file_path = Path(secrets_path, file_path)
+        contains_secrets = True
+    elif node.tag == '!scs-common':
+        file_path = Path(common_path, file_path)
+    else:
+        file_path = Path(loader.filepath.absolute().parent, file_path)
+
+    file_data = load_yaml(file_path, secrets=contains_secrets)
 
     if not attribute_loc:
         return file_data
@@ -61,16 +60,55 @@ def construct_scs_ref(loader, node):
     return level_data
 
 
-yaml.SafeLoader.add_constructor('!scs-ref', construct_scs_ref)
+def construct_secret(loader, node):
+    """
+    Constructs a random secret for a node, using the secrets.token_urlsafe
+    function
+    """
+    loader.file_properties['secrets_parsed'] = True
+
+    return secrets.token_urlsafe(32)
 
 
-def valid_config_path(relative_path, parser):
-    """Check if the given path is valid and return the path"""
-    path = Path(config_files_path, relative_path)
-    if path.is_file() and not path.name.endswith('scs-env.yaml'):
-        return relative_path
-    else:
-        parser.error('Invalid path provided!')
+def get_env_var(match):
+    """
+    Return the environment variable for the 'env_variable_pattern' match
+    """
+    env_var_name = match.group(1)
+    try:
+        return os.environ[env_var_name]
+    except KeyError:
+        raise KeyError(f'Environment Variable {env_var_name} not defined!')\
+            from None
+
+
+def expand_env_vars(loader, node):
+    """
+    Constructs a random secret for a node, using the secrets.token_urlsafe
+    function
+    """
+    return env_variable_pattern.sub(get_env_var, node.value)
+
+
+class BaseSCSConfigLoader(yaml.SafeLoader):
+    # To keep constructors different for secrets files and other files
+    pass
+
+
+BaseSCSConfigLoader.add_constructor('!scs-relative', construct_scs_ref)
+BaseSCSConfigLoader.add_constructor('!scs-secret', construct_scs_ref)
+BaseSCSConfigLoader.add_constructor('!scs-common', construct_scs_ref)
+BaseSCSConfigLoader.add_constructor('!scs-expand-env', expand_env_vars)
+
+
+class BaseSCSSecretsLoader(yaml.SafeLoader):
+    # This class will only have a constructor to generate secrets, and variable
+    # To indicate if they have been generated
+    pass
+
+
+BaseSCSSecretsLoader.add_constructor('!scs-gen-secret', construct_secret)
+BaseSCSConfigLoader.add_constructor('!scs-secret', construct_scs_ref)
 
 
 def get_args():
@@ -79,18 +117,41 @@ def get_args():
         description="Script to test the parsing of configuration files"
     )
     aparser.add_argument(
+        'settings_folder',
+        help=(
+            "Path of the folder containing the 'scs_auth' and 'scs_conf'"
+            " files"
+        ),
+        type=str
+    )
+    aparser.add_argument(
         'path',
         help="Path, relative to the config file directory",
-        type=lambda x: valid_config_path(x, aparser)
+        type=str
     )
     args = aparser.parse_args()
     return args
 
 
-def load_yaml(path):
+def load_yaml(path, secrets=False):
     """Load the YAML data from the given path"""
+    # A class is created for each load, so variables can be stored, to be
+    # passed to the parsing function, and/or read afterwards
+    if secrets:
+        class Loader(BaseSCSSecretsLoader):
+            file_properties = {'secrets_parsed': False}
+    else:
+        class Loader(BaseSCSConfigLoader):
+            filepath = path
+
     with open(path, 'r', encoding='utf-8') as yaml_file:
-        return yaml.safe_load(yaml_file)
+        data = yaml.load(yaml_file, Loader)
+
+    if secrets and Loader.file_properties['secrets_parsed']:
+        with open(path, 'w', encoding='utf8') as yaml_file:
+            yaml.dump(data, yaml_file, sort_keys=False)
+
+    return data
 
 
 def get_env(relative_path):
@@ -98,7 +159,7 @@ def get_env(relative_path):
     Get all environment variables for the given path
     """
     # Note: At this moment only loads the accompanying scs-env file
-    path = Path(config_files_path, relative_path)
+    path = Path(config_path, relative_path)
     env_file_name = path.name + '.scs-env.yaml'
     env_file_path = Path(path.parent, env_file_name)
 
@@ -118,10 +179,33 @@ def render_config_file(relative_path, env):
 
 
 def main():
+    global config_path, common_path, secrets_path, jinja_env
     args = get_args()
 
-    env = get_env(args.path)
+    # First load the configuration
+    print('################ Configuration ###################')
+    settings_path = Path(file_folder, args.settings_folder)
+    config_data = load_yaml(Path(settings_path, 'scs_conf.yaml'))
+    print(config_data)
 
+    config_path = config_data['directories']['config']
+    common_path = config_data['directories']['common']
+    secrets_path = config_data['directories']['secrets']
+    loader = jinja2.FileSystemLoader(
+        searchpath=config_path
+    )
+    jinja_env = jinja2.Environment(
+        loader=loader,
+        trim_blocks=True,
+        lstrip_blocks=True
+    )
+
+    # Load the user data
+    print('################ Accounts ###################')
+    print(load_yaml(Path(settings_path, 'scs_auth.yaml')))
+
+    env = get_env(args.path)
+    print('################ Config Data ###################')
     print(render_config_file(args.path, env))
 
 
