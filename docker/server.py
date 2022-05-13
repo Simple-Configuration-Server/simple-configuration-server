4280a8bf7471ecedff52e8ae4bbaa9ef09eeccf8
Tom Brouwer <tombrouwer@outlook.com>
2022-05-13 20:12:46 +0200

    docker/server.py: Restructure to ensure execution only when used as script

--- docker/server.py
+++ docker/server.py
@@ -1,3 +1,7 @@
+"""
+Main server for SCS, using the Cheroot HTTP server. This script is designed to
+be used inside the simple-configuration-server docker image
+"""
 import signal
 import os
 
@@ -14,28 +18,27 @@ def sigterm_handler(*args):
     raise KeyboardInterrupt('Docker SIGTERM was sent')
 
 
-signal.signal(signal.SIGTERM, sigterm_handler)
-flask_app = create_app()
-cheroot_app = WSGIPathInfoDispatcher({'/': flask_app})
-
-disable_scs_ssl = bool(int(os.environ.get("DISABLE_SCS_SSL", "0")))
-if disable_scs_ssl:
-    message = (
-        'DISABLE_SCS_SSL has been enabled, meaning INSECURE HTTP connections '
-        'are used. Use this only if you plan to have SSL terminated by a proxy'
-        ' like NGINX.'
-    )
-    flask_app.logger.warning(message)
-    print(f'WARNING: {message}', flush=True)
-    server = WSGIServer(('0.0.0.0', 80), cheroot_app)
-else:
-    server = WSGIServer(('0.0.0.0', 443), cheroot_app)
-    server.ssl_adapter = BuiltinSSLAdapter(
-        certificate='/etc/ssl/certs/scs.crt',
-        private_key='/etc/ssl/private/scs.key',
-    )
-
 if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    flask_app = create_app()
+    cheroot_app = WSGIPathInfoDispatcher({'/': flask_app})
+
+    disable_scs_ssl = bool(int(os.environ.get("DISABLE_SCS_SSL", "0")))
+    if disable_scs_ssl:
+        message = (
+            'DISABLE_SCS_SSL has been enabled, meaning INSECURE HTTP connections '
+            'are used. Use this only if you plan to have SSL terminated by a proxy'
+            ' like NGINX.'
+        )
+        flask_app.logger.warning(message)
+        print(f'WARNING: {message}', flush=True)
+        server = WSGIServer(('0.0.0.0', 80), cheroot_app)
+    else:
+        server = WSGIServer(('0.0.0.0', 443), cheroot_app)
+        server.ssl_adapter = BuiltinSSLAdapter(
+            certificate='/etc/ssl/certs/scs.crt',
+            private_key='/etc/ssl/private/scs.key',
+        )
     try:
         message = 'Simple Configuration Server Started'
         print(message, flush=True)
