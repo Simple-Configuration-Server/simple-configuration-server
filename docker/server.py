b7bcf44fe89630ef021d4da4d59ee8ef4aff9e2c
Tom Brouwer <tombrouwer@outlook.com>
2022-06-10 15:04:12 +0200

    Add support for using SCS behind reverse proxies
    By setting the SCS_REVERSE_PROXY_COUNT environment variable, you
    can indicate if the server is running behind one or more
    proxies. This is relevant, since behind a proxy, the remote
    address needs to be determined from the 'X-Forwarded-For' header.
    The count indicates how many values to expect in the header. If
    using one proxy, only 1 is expected. This prevents a client from
    being able to spoof it's IP by setting a X-Forwarded-For header on
    the first request to the reverse proxy.
    
    Note that also the DISABLE_SCS_SSL has been renamed to
    renamed to SCS_DISABLE_SSL for consistency.

--- docker/server.py
+++ docker/server.py
@@ -8,6 +8,7 @@ import os
 from cheroot.wsgi import Server as WSGIServer
 from cheroot.wsgi import PathInfoDispatcher as WSGIPathInfoDispatcher
 from cheroot.ssl.builtin import BuiltinSSLAdapter
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from scs import create_app
 
@@ -18,15 +19,48 @@ def sigterm_handler(*args):
     raise KeyboardInterrupt('Docker SIGTERM was sent')
 
 
+def get_environment_config() -> dict:
+    """
+    Loads the part of the configuration that's stored in environment variables.
+
+    The following environment variables are loaded:
+        SCS_DISABLE_SSL:
+            Set to 1 in case you want to disable HTTPS and use port 80 (
+            default: 0)
+        SCS_REVERSE_PROXY_COUNT:
+            Set to >= 1 if you're using a reverse proxy that sets the
+            X-Forwarded-For header. This means the 'X-Forwarded-For' header is
+            used as the Remote Address, rather than the IP of th proxy itself.
+            The count indicates how many values should be in the
+            X-Forwarded-For headers. If you use multiple proxies, this may be
+            higher than one. (default: 0)
+    """
+    return {
+        'disable_ssl': bool(int(os.environ.get("SCS_DISABLE_SSL", "0"))),
+        'reverse_proxy_count': int(
+            os.environ.get("SCS_REVERSE_PROXY_COUNT", "0")
+        ),
+    }
+
+
 if __name__ == '__main__':
     signal.signal(signal.SIGTERM, sigterm_handler)
     flask_app = create_app()
-    cheroot_app = WSGIPathInfoDispatcher({'/': flask_app})
 
-    disable_scs_ssl = bool(int(os.environ.get("DISABLE_SCS_SSL", "0")))
-    if disable_scs_ssl:
+    env_config = get_environment_config()
+    if env_config['reverse_proxy_count'] > 0:
+        app = ProxyFix(
+            flask_app,
+            x_for=env_config['reverse_proxy_count'],
+        )
+    else:
+        app = flask_app
+
+    cheroot_app = WSGIPathInfoDispatcher({'/': app})
+
+    if env_config['disable_ssl']:
         message = (
-            'DISABLE_SCS_SSL has been enabled, meaning INSECURE HTTP'
+            'SCS_DISABLE_SSL has been enabled, meaning INSECURE HTTP'
             ' connections are used. Use this only if you plan to have SSL'
             ' terminated by a proxy like NGINX.'
         )
