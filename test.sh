cf3b23fe69402bc654fb14692c9f51f35e8181a8
Tom Brouwer <tombrouwer@outlook.com>
2022-07-08 15:49:43 +0200

    Add CI/CD stage for testing and add python version consistency check
    The same tests are used for pre-commit and CI/CD

--- test.sh
+++ test.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+set -e
+
+# Validate python version consistency in repository
+# Dockerfile and CI/CD
+PYTHON_VERSION=3.10
+
+DOCKERFILE_CONTENTS=$(cat Dockerfile)
+CICDFILE_CONTENTS=$(cat .gitlab-ci.yml)
+INSTALL_CONTENTS=$(cat install.sh)
+DEV_INSTALL_CONTENTS=$(cat install.dev.sh)
+
+if [[ $DOCKERFILE_CONTENTS != *"FROM python:$PYTHON_VERSION "* ]]; then
+  echo "ERROR: Dockerfile python version inconsistent with test.sh!"
+  exit 1
+fi
+
+if [[ $CICDFILE_CONTENTS != *"image: python:$PYTHON_VERSION"* ]]; then
+  echo "ERROR: .gitlab-ci.yml python version inconsistent with test.sh!"
+  exit 1
+fi
+
+if [[ $INSTALL_CONTENTS != *"python$PYTHON_VERSION "* ]]; then
+  echo "ERROR: install.sh python version inconsistent with test.sh!"
+  exit 1
+fi
+
+if [[ $DEV_INSTALL_CONTENTS != *"python$PYTHON_VERSION "* ]]; then
+  echo "ERROR: install.dev.sh python version inconsistent with test.sh!"
+  exit 1
+fi
+
+echo "Python version consistency check: PASSED"
+echo " "
 
 # Tests are run this way, so each file is seperately tested. We cannot re-use
 # loaded modules accross tests, because the modules use global variables
